68d0b0e8b897570c06ec8376ef90af35c09028aa
mohamed <sickmido@gmail.com>
2025-06-03 07:35:47 +0300

    a

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ const ListsRouter = require("./src/routes/listsRouter");
 const authMiddleware = require("./src/middleware/authMiddleware");
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
 
 app.use("/api/auth", authRouter);
 app.use("/api/lists", authMiddleware, ListsRouter);
